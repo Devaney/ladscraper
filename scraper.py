5e6a398cdeef84826ef5ec83e5373b995e13c2e4
Devaney <liamdevaney@live.co.uk>
2017-11-18 01:18:20 +0000

    Playing with scraps output

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -8,18 +9,27 @@ class LadSpidyTest(CrawlSpider):
         'http://www.memset.com/',
     )
     rules = (
-        Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_obj", follow=True),
+        Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_obj", follow=False),
     )
 
     def parse_obj(self, response):
-        extractor = LinkExtractor(allow=self.allowed_domains)
+        extractor = LinkExtractor(allow=self.allowed_domains, unique=True)
         links = extractor.extract_links(response)
         url_list = []
-        for link in links:
-            #print link.url
-            #url_list.append(link.url)
-            if link.url not in url_list:
-                url_list.append(link.url)
+        print url_list
+        print "print before loooop"
+        for i in links:
+            #print ("Printing link ")
+            #print i.url
+            url_list.append(i.url)
+            print "print DURING loooop"
+            #print ("Printing LIST ")
+            #print len(url_list)
+            #print ("printing request")
+            #print scrapy.Request(url=i.url)
+        print "print afteerrr loooop"
+        print url_list
 
-        unique_url_list = set(url_list)
-        print unique_url_list
+    #def print_lists():
+    #    unique_url_list = set(self.url_list)
+    #    print unique_url_list
