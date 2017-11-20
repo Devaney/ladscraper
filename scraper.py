beb77d70268fa921c1d4c6e5671570678f4d65a3
Devaney <liamdevaney@live.co.uk>
2017-11-20 20:39:35 +0000

    Spider is getting urls and on close calls them for printing
    FIXME  Not to happy about having a global list..

--- scraper.py
+++ scraper.py
@@ -2,6 +2,8 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+url_list = []
+
 class LadSpidyTest(CrawlSpider):
     name = "lad_scraper"
     allowed_domains = ["www.memset.com"]
@@ -9,27 +11,19 @@ class LadSpidyTest(CrawlSpider):
         'http://www.memset.com/',
     )
     rules = (
-        Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_obj", follow=False),
+        Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_obj", follow=True),
     )
 
     def parse_obj(self, response):
         extractor = LinkExtractor(allow=self.allowed_domains, unique=True)
         links = extractor.extract_links(response)
-        url_list = []
-        print url_list
-        print "print before loooop"
         for i in links:
-            #print ("Printing link ")
-            #print i.url
             url_list.append(i.url)
-            print "print DURING loooop"
-            #print ("Printing LIST ")
-            #print len(url_list)
-            #print ("printing request")
-            #print scrapy.Request(url=i.url)
-        print "print afteerrr loooop"
-        print url_list
 
-    #def print_lists():
-    #    unique_url_list = set(self.url_list)
-    #    print unique_url_list
+    def closed(self, reason):
+        completeList()
+
+def completeList():
+    url_set = set(url_list)
+    print len(url_list)
+    print len(url_set)
